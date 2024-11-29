e9bed0d22e3f121011e11cf51f13d4c7f046cd9d
Zaid-NA3120 <kamelahmad3120@gmail.com>
2024-11-29 03:43:15 +0300

    Update kalman_estimator.py

--- kalman_estimator.py
+++ kalman_estimator.py
@@ -4,10 +4,23 @@ from scipy.linalg import expm
 
 
 
-def create_velocity_model(b, m, r, L, I, Ts, sigma_r, sigma_l):
+def create_velocity_model(b, m, r, L, I,Iw ,Ts,d, mc,sigma_r, sigma_l,state):
+    wk = state[2]
+    vk = state[3]
+    a12 = float(- (mc * d* wk *Ts)/ (I + 2*L**2 * Iw/r**2))
+    a11 = float(1 - (mc * d* vk *Ts)/ (I + 2*L**2 * Iw/r**2))
+    a22 = 1.0
+    a21 = float((2* mc * d *wk * Ts) / (m + 2*Iw/r**2))
 
-    A_dis = np.array([[0, 0, 1, 0],[0, 0, 0, 1],[0, 0, 1, 0],[0, 0, 0, np.exp(-b*Ts/m)]])
-    B_dis = np.array ([[0,0],[0, 0],[(m*L*Ts)/(2*I), -(m*L*Ts)/(2*I)],[r*m*(1-np.exp(-b*Ts/m))/(2*b), r*m*(1-np.exp(-b*Ts/m))/(2*b)]])
+    print(f"({float(a11)}, {a12}, {a21}, {a22})")
+
+    b21 = Ts/(m + 2*Iw/r**2)/r 
+    b22  =Ts/(m + 2*Iw/r**2)/r 
+    b11 = L* Ts/r /(I + 2*L**2 * Iw/r**2)
+    b12 = - L* Ts/r /(I + 2*L**2 * Iw/r**2) 
+
+    A_dis = np.array([[0,0,1,0],[0,0,0,1],[0, 0, a11, a12],[0, 0, a21, a22]])
+    B_dis = np.array ([[0,0],[0, 0],[b11, b12],[b21, b22]])
     Sigma_matrix = np.array([[sigma_r**2, 0],[0, sigma_l**2]])
     Noise_Cov = B_dis @ Sigma_matrix @ B_dis.T
     return A_dis, B_dis, Noise_Cov
@@ -17,8 +30,7 @@ def create_measurement_model (Ts,sigma_a, sigma_w):
     Noise_Cov = np.array([[sigma_a**2, 0],[0, sigma_w**2]])
     return H, Noise_Cov
 
-A,B,Q = create_velocity_model(0.1,2,0.3,0.4,0.1,0.01,0.1,0.2)
-H, Q = create_measurement_model(0.1,0.1,0.2)
+
 
 
 def motion_model (v, w, x, y, theta, Ts):
@@ -32,8 +44,17 @@ def motion_model (v, w, x, y, theta, Ts):
 
     return x_new, y_new, theta_new
      
-def Kalman_filter_step(A,B,Q,H,R,x_last,u,z,Cov,correction_flag = True):
-    x_prediction = A @ x_last + B @ u
+def Kalman_filter_step(A,B,Q,H,R,x_last,u,z,Cov,Ts,I,Iw,r,mc,L,m,d,correction_flag = True,prediction_flag = True):
+    if prediction_flag == False:
+        x_new = np.array([[float(x_last[2])],[float(x_last[3])],[0],[0]])
+        x_new[2] = z[1]
+        x_new[3] = z[0]*Ts +x_last[3]
+        return x_new, Cov
+
+    
+    x_prediction = np.array([[float(x_last[2])],[float(x_last[3])],[0],[0]])
+    x_prediction[2] = float(((L/r)*(u[0]-u[1]) - mc*d*x_last[2]*x_last[3]) * Ts/(I + 2*L**2 * Iw/r**2) + x_last[2])
+    x_prediction[3] = float(((1/r)*(u[0]+u[1]) + mc*d*x_last[2]**2) * Ts/(m + 2* Iw/r**2) + x_last[3])
     Cov_prediction = A @ Cov @ A.T + Q 
     if correction_flag:
         S = H @ Cov_prediction @ H.T + R
@@ -41,9 +62,10 @@ def Kalman_filter_step(A,B,Q,H,R,x_last,u,z,Cov,correction_flag = True):
 
         x_new = x_prediction + K @ (z - H @ x_prediction)
         Cov_new = (np.identity(4) - K @ H) @ Cov_prediction
+        return x_new, Cov_new
     else:
         return x_prediction, Cov_prediction
 
-    return x_new, Cov_new
+    
 
     
